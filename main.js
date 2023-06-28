748709f7c996910f371060665cd90d16c25b3c89
Thanakorn Waleejaroenpong <thanakornwaleejaroenpong@MacBook-Pro-khxng-Thanakorn.local>
2023-06-28 23:19:11 +0700

    4.9

--- main.js
+++ main.js
@@ -6,8 +6,17 @@ createApp({
         const description = ref('Very good boots')
         const image = ref('./assets/images/socks_green.jpg')
         const camtWebsite = ref('https://www.camt.cmu.ac.th/')
+        const inStock = ref(false)
+        const inventory = ref(100)
+        const onSale = ref(true)
         return {
-            product, description, image, camtWebsite
+            product, 
+            description, 
+            image, 
+            camtWebsite, 
+            inStock, 
+            inventory,
+            onSale
         }
     }
 }).mount('#app')
