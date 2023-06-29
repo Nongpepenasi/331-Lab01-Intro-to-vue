a98a3cf6c11daa3c2d0e659097e6083684852bc9
Thanakorn Waleejaroenpong <thanakornwaleejaroenpong@MacBook-Pro-khxng-Thanakorn.local>
2023-06-29 22:27:54 +0700

    8.5

--- main.js
+++ main.js
@@ -1,35 +1,61 @@
-const { createApp, ref } = Vue
+const { createApp, ref, computed } = Vue
 
 createApp({
     setup(){
         const product = ref('Boots')
+        const brand = ref('SE 331')
         const description = ref('Very good boots')
-        const image = ref('./assets/images/socks_green.jpg')
+        // const image = ref('./assets/images/socks_green.jpg')
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image
+        })
         const camtWebsite = ref('https://www.camt.cmu.ac.th/')
-        const inStock = ref(true)
+        // const inStock = ref(true)
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity
+        })
+
         const inventory = ref(100)
+
         const onSale = ref(true)
+
         const details = ref([
             '50% cotton',
             '30% wool',
             '20% polyester'
         ])
+
         const variants = ref([
-            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
         ])
+
+        const selectedVariant = ref(0)
+
         const sockSizes = ref([
             'S',
             'M',
             'L'
         ])
+
         const cart = ref(0)
+
         function addToCart() {
             cart.value += 1
         }
+
+        const title = computed(() => {
+            if (onSale.value) {
+                return brand.value + ' ' + product.value + " is on sale"
+            } else {
+                return brand.value + ' ' + product.value
+            }
+        })
+
         function updateImage(variantImage){
             image.value = variantImage
         }
+
         function updateStock(){
             if (inStock.value){
                 inStock.value = false;
@@ -37,8 +63,13 @@ createApp({
                 inStock.value = true;
             }
         }
+
+        function updateVariant(index) {
+            selectedVariant.value = index;
+        }
+
         return {
-            product, 
+            title,
             description, 
             image, 
             camtWebsite, 
@@ -51,7 +82,8 @@ createApp({
             cart,
             addToCart,
             updateImage,
-            updateStock
+            updateStock,
+            updateVariant
         }
     }
 }).mount('#app')
