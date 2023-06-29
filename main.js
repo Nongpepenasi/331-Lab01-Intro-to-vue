40e9a148d633d854979b2d29abc6c10639371562
Thanakorn Waleejaroenpong <thanakornwaleejaroenpong@MacBook-Pro-khxng-Thanakorn.local>
2023-06-29 21:36:41 +0700

    6.7

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ createApp({
         const description = ref('Very good boots')
         const image = ref('./assets/images/socks_green.jpg')
         const camtWebsite = ref('https://www.camt.cmu.ac.th/')
-        const inStock = ref(false)
+        const inStock = ref(true)
         const inventory = ref(100)
         const onSale = ref(true)
         const details = ref([
@@ -15,14 +15,28 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            {id: 2234, color: 'green'},
-            {id: 2235, color: 'blue'}
+            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
         ])
         const sockSizes = ref([
             'S',
             'M',
             'L'
         ])
+        const cart = ref(0)
+        function addToCart() {
+            cart.value += 1
+        }
+        function updateImage(variantImage){
+            image.value = variantImage
+        }
+        function updateStock(){
+            if (inStock.value){
+                inStock.value = false;
+            } else {
+                inStock.value = true;
+            }
+        }
         return {
             product, 
             description, 
@@ -33,7 +47,11 @@ createApp({
             onSale,
             details,
             variants,
-            sockSizes
+            sockSizes,
+            cart,
+            addToCart,
+            updateImage,
+            updateStock
         }
     }
 }).mount('#app')
